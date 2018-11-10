9e4a0cbf7122c7da05f3e147ddf035749a0afe5d
zhangshuo <zhangshuo@bbktel.com>
2018-11-10 17:22:09 +0800

    add await timeout

--- src/main/java/com/vivo/internet/fasterTask/concurrent/TaskProcess.java
+++ src/main/java/com/vivo/internet/fasterTask/concurrent/TaskProcess.java
@@ -30,281 +30,284 @@ import com.vivo.internet.fasterTask.concurrent.task.TaskAction;
  */
 @SuppressWarnings("unchecked")
 public class TaskProcess {
-    private static Logger logger = LoggerFactory.getLogger(TaskProcess.class);
+	private static Logger logger = LoggerFactory.getLogger(TaskProcess.class);
 
-    private ExecutorService executor;
+	private ExecutorService executor;
 
-    private int coreSize;
+	private int coreSize;
 
-    private int poolSize;
+	private int poolSize;
 
-    private String domain;
+	private String domain;
 
-    public TaskProcess(String domain, int coreSize, int poolSize) {
-        this.coreSize = coreSize;
-        this.poolSize = poolSize;
-        this.domain = domain;
-        init();
-    }
+	public TaskProcess(String domain, int coreSize, int poolSize) {
+		this.coreSize = coreSize;
+		this.poolSize = poolSize;
+		this.domain = domain;
+		init();
+	}
 
-    public TaskProcess(ExecutorService executor) {
-        this.executor = executor;
-        addHook();
-    }
+	public TaskProcess(ExecutorService executor) {
+		this.executor = executor;
+		addHook();
+	}
 
-    private void createThreadPool() {
-        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(coreSize);
-        executor = new ThreadPoolExecutor(coreSize, poolSize, 60, TimeUnit.SECONDS, queue,
-                new DefaultThreadFactory(domain), new ThreadPoolExecutor.CallerRunsPolicy());
-    }
+	private void createThreadPool() {
+		BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(coreSize);
+		executor = new ThreadPoolExecutor(coreSize, poolSize, 60, TimeUnit.SECONDS, queue,
+				new DefaultThreadFactory(domain), new ThreadPoolExecutor.CallerRunsPolicy());
+	}
 
-    private void addHook() {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                if (executor == null) {
-                    return;
-                }
-                try {
-                    executor.shutdown();
-                    executor.awaitTermination(5, TimeUnit.MINUTES);
-                } catch (InterruptedException e) {
-                    logger.warn("interrupted when shuting down the process executor:\n{}", e);
-                }
-            }
-        });
-    }
+	private void addHook() {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				if (executor == null) {
+					return;
+				}
+				try {
+					executor.shutdown();
+					executor.awaitTermination(5, TimeUnit.MINUTES);
+				} catch (InterruptedException e) {
+					logger.warn("interrupted when shuting down the process executor:\n{}", e);
+				}
+			}
+		});
+	}
 
-    private void init() {
-        createThreadPool();
-        addHook();
-    }
+	private void init() {
+		createThreadPool();
+		addHook();
+	}
 
-    public <T> List<T> executeTask(List<TaskAction<T>> tasks) {
-        TaskAction<T>[] actions = new TaskAction[tasks.size()];
-        for (int i = 0; i < tasks.size(); i++) {
-            actions[i] = tasks.get(i);
-        }
-        return executeTask(actions);
-    }
+	public <T> List<T> executeTask(List<TaskAction<T>> tasks) {
+		TaskAction<T>[] actions = new TaskAction[tasks.size()];
+		for (int i = 0; i < tasks.size(); i++) {
+			actions[i] = tasks.get(i);
+		}
+		return executeTask(actions);
+	}
 
-    /**
-     * 
-     * @param tasks
-     * @return
-     */
-    public <T> List<T> executeTask(TaskAction<T>... tasks) {
-        final CountDownLatch latch = new CountDownLatch(tasks.length);
+	/**
+	 * 
+	 * @param tasks
+	 * @return
+	 */
+	public <T> List<T> executeTask(TaskAction<T>... tasks) {
+		final CountDownLatch latch = new CountDownLatch(tasks.length);
 
-        List<Future<T>> futures = new ArrayList<Future<T>>();
-        List<T> resultList = new ArrayList<T>();
+		List<Future<T>> futures = new ArrayList<Future<T>>();
+		List<T> resultList = new ArrayList<T>();
 
-        for (final TaskAction<T> runnable : tasks) {
-            Future<T> future = executor.submit(new Callable<T>() {
-                @Override
-                public T call() throws Exception {
-                    try {
-                        return runnable.doInAction();
-                    } finally {
-                        latch.countDown();
-                    }
-                }
-            });
-            futures.add(future);
-        }
+		for (final TaskAction<T> runnable : tasks) {
+			Future<T> future = executor.submit(new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					try {
+						return runnable.doInAction();
+					} finally {
+						latch.countDown();
+					}
+				}
+			});
+			futures.add(future);
+		}
 
-        try {
-            latch.await();// block if all tasks be finished!
-        } catch (InterruptedException e) {
-            logger.info("Executing Task is interrupt.");
-        }
+		try {
+			latch.await(10, TimeUnit.SECONDS);// block if all tasks be finished!
+		} catch (InterruptedException e) {
+			logger.info("Executing Task is interrupt.");
+		}
 
-        for (Future<T> future : futures) {
-            try {
-                T result = future.get();// wait
-                if (result != null) {
-                    resultList.add(result);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return resultList;
-    }
+		for (Future<T> future : futures) {
+			try {
+				T result = future.get();// wait
+				if (result != null) {
+					resultList.add(result);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return resultList;
+	}
 
-    /**
-     * 异步执行任务
-     * @param tasks
-     */
-    public <T> void asyncExecuteTask(List<TaskAction<T>> tasks) {
-        TaskAction<T>[] actions = new TaskAction[tasks.size()];
-        for (int i = 0; i < tasks.size(); i++) {
-            actions[i] = tasks.get(i);
-        }
-        asyncExecuteTask(actions);
-    }
+	/**
+	 * 异步执行任务
+	 * 
+	 * @param tasks
+	 */
+	public <T> void asyncExecuteTask(List<TaskAction<T>> tasks) {
+		TaskAction<T>[] actions = new TaskAction[tasks.size()];
+		for (int i = 0; i < tasks.size(); i++) {
+			actions[i] = tasks.get(i);
+		}
+		asyncExecuteTask(actions);
+	}
 
-    public void asyncExecuteTask(TaskAction<?>... tasks) {
-        for (final TaskAction<?> runnable : tasks) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        runnable.doInAction();
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });
-        }
-    }
+	public void asyncExecuteTask(TaskAction<?>... tasks) {
+		for (final TaskAction<?> runnable : tasks) {
+			executor.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						runnable.doInAction();
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				}
+			});
+		}
+	}
 
-    /**
-     * @param concurrentCount 并行执行的最大任务数
-     * @param tasks 
-     * @return
-     */
-    public <T> List<T> exeucteTaskByLatch(int concurrentCount, List<TaskAction<T>> tasks) {
-        TaskAction<T>[] actions = new TaskAction[tasks.size()];
-        for (int i = 0; i < tasks.size(); i++) {
-            actions[i] = tasks.get(i);
-        }
-        return exeucteTaskByLatch(concurrentCount, actions);
-    }
+	/**
+	 * @param concurrentCount
+	 *            并行执行的最大任务数
+	 * @param tasks
+	 * @return
+	 */
+	public <T> List<T> exeucteTaskByLatch(int concurrentCount, List<TaskAction<T>> tasks) {
+		TaskAction<T>[] actions = new TaskAction[tasks.size()];
+		for (int i = 0; i < tasks.size(); i++) {
+			actions[i] = tasks.get(i);
+		}
+		return exeucteTaskByLatch(concurrentCount, actions);
+	}
 
-    public <T> List<T> exeucteTaskByLatch(int concurrentCount, TaskAction<T>... tasks) {
-        final BarrierLatch latch = new BarrierLatch(concurrentCount);
+	public <T> List<T> exeucteTaskByLatch(int concurrentCount, TaskAction<T>... tasks) {
+		final BarrierLatch latch = new BarrierLatch(concurrentCount);
 
-        List<Future<T>> futures = new ArrayList<Future<T>>();
-        List<T> resultList = new ArrayList<T>();
+		List<Future<T>> futures = new ArrayList<Future<T>>();
+		List<T> resultList = new ArrayList<T>();
 
-        for (final TaskAction<T> runnable : tasks) {
-            try {
-                latch.acquire();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Future<T> future = executor.submit(new Callable<T>() {
-                @Override
-                public T call() throws Exception {
-                    try {
-                        return runnable.doInAction();
-                    } finally {
-                        latch.release();
-                    }
-                }
-            });
-            futures.add(future);
-        }
+		for (final TaskAction<T> runnable : tasks) {
+			try {
+				latch.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			Future<T> future = executor.submit(new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					try {
+						return runnable.doInAction();
+					} finally {
+						latch.release();
+					}
+				}
+			});
+			futures.add(future);
+		}
 
-        for (Future<T> future : futures) {
-            try {
-                T result = future.get();// wait
-                if (result != null) {
-                    resultList.add(result);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return resultList;
-    }
+		for (Future<T> future : futures) {
+			try {
+				T result = future.get();// wait
+				if (result != null) {
+					resultList.add(result);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return resultList;
+	}
 
-    /**
-     * 标识性任务执行，等待执行结果，对任务执行结果分类
-     * @param tasks
-     * @return
-     */
-    public <T> Map<String, Object> executeIdentityTask(List<IdentityTaskAction<Object>> tasks) {
-        IdentityTaskAction<Object>[] actions = new IdentityTaskAction[tasks.size()];
-        for (int i = 0; i < tasks.size(); i++) {
-            actions[i] = tasks.get(i);
-        }
-        return executeIdentityTask(actions);
-    }
+	/**
+	 * 标识性任务执行，等待执行结果，对任务执行结果分类
+	 * 
+	 * @param tasks
+	 * @return
+	 */
+	public <T> Map<String, Object> executeIdentityTask(List<IdentityTaskAction<Object>> tasks) {
+		IdentityTaskAction<Object>[] actions = new IdentityTaskAction[tasks.size()];
+		for (int i = 0; i < tasks.size(); i++) {
+			actions[i] = tasks.get(i);
+		}
+		return executeIdentityTask(actions);
+	}
 
-    public <T> Map<String, T> executeIdentityTask(IdentityTaskAction<T>... tasks) {
-        final CountDownLatch latch = new CountDownLatch(tasks.length);
+	public <T> Map<String, T> executeIdentityTask(IdentityTaskAction<T>... tasks) {
+		final CountDownLatch latch = new CountDownLatch(tasks.length);
 
-        Map<String, Future<T>> futures = new HashMap<String, Future<T>>();
-        Map<String, T> resultMap = new HashMap<String, T>();
+		Map<String, Future<T>> futures = new HashMap<String, Future<T>>();
+		Map<String, T> resultMap = new HashMap<String, T>();
 
-        for (final IdentityTaskAction<T> runnable : tasks) {
-            Future<T> future = executor.submit(new Callable<T>() {
-                @Override
-                public T call() throws Exception {
-                    long time = System.currentTimeMillis();
-                    try {
-                        return runnable.doInAction();
-                    } finally {
-                        logger.debug("Executing Task : {} ,time :{}", runnable.identity(),
-                                System.currentTimeMillis() - time);
-                        latch.countDown();
-                    }
+		for (final IdentityTaskAction<T> runnable : tasks) {
+			Future<T> future = executor.submit(new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					long time = System.currentTimeMillis();
+					try {
+						return runnable.doInAction();
+					} finally {
+						logger.debug("Executing Task : {} ,time :{}", runnable.identity(),
+								System.currentTimeMillis() - time);
+						latch.countDown();
+					}
 
-                }
-            });
-            futures.put(runnable.identity(), future);
-        }
+				}
+			});
+			futures.put(runnable.identity(), future);
+		}
 
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            logger.info("Executing Task is interrupt.");
-        }
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			logger.info("Executing Task is interrupt.");
+		}
 
-        Iterator<Entry<String, Future<T>>> it = futures.entrySet().iterator();
-        while (it.hasNext()) {
-            Entry<String, Future<T>> entry = it.next();
-            try {
-                T result = entry.getValue().get();
-                resultMap.put(entry.getKey(), result);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return resultMap;
-    }
+		Iterator<Entry<String, Future<T>>> it = futures.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<String, Future<T>> entry = it.next();
+			try {
+				T result = entry.getValue().get();
+				resultMap.put(entry.getKey(), result);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return resultMap;
+	}
 
-    /**
-     * The default thread factory
-     */
-    static class DefaultThreadFactory implements ThreadFactory {
-        static final AtomicInteger poolNumber = new AtomicInteger(1);
+	/**
+	 * The default thread factory
+	 */
+	static class DefaultThreadFactory implements ThreadFactory {
+		static final AtomicInteger poolNumber = new AtomicInteger(1);
 
-        final ThreadGroup group;
+		final ThreadGroup group;
 
-        final AtomicInteger threadNumber = new AtomicInteger(1);
+		final AtomicInteger threadNumber = new AtomicInteger(1);
 
-        final String namePrefix;
+		final String namePrefix;
 
-        DefaultThreadFactory(String domain) {
-            SecurityManager s = System.getSecurityManager();
-            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-            namePrefix = domain + "-TaskProcess-" + poolNumber.getAndIncrement() + "-thread-";
-        }
+		DefaultThreadFactory(String domain) {
+			SecurityManager s = System.getSecurityManager();
+			group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+			namePrefix = domain + "-TaskProcess-" + poolNumber.getAndIncrement() + "-thread-";
+		}
 
-        public Thread newThread(Runnable r) {
-            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-            if (t.isDaemon())
-                t.setDaemon(false);
-            if (t.getPriority() != Thread.NORM_PRIORITY)
-                t.setPriority(Thread.NORM_PRIORITY);
-            return t;
-        }
-    }
+		public Thread newThread(Runnable r) {
+			Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+			if (t.isDaemon())
+				t.setDaemon(false);
+			if (t.getPriority() != Thread.NORM_PRIORITY)
+				t.setPriority(Thread.NORM_PRIORITY);
+			return t;
+		}
+	}
 
-    public <T> Holder<T> syncExecuteWaiting(final TaskAction<T> task) {
-        Future<T> future = executor.submit(new Callable<T>() {
-            @Override
-            public T call() throws Exception {
-                try {
-                    return task.doInAction();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        return new Holder<T>(future);
-    }
+	public <T> Holder<T> syncExecuteWaiting(final TaskAction<T> task) {
+		Future<T> future = executor.submit(new Callable<T>() {
+			@Override
+			public T call() throws Exception {
+				try {
+					return task.doInAction();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
+		return new Holder<T>(future);
+	}
 }
